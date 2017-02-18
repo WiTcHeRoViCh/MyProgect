7030986d38ec09c3e43f782e14f7b5779f4a9adb
IlyaUmanets <strateg2claria@gmail.com>
2017-02-18 22:27:36 +0200

    Fix active admin issue

--- Social_N/app/controllers/application_controller.rb
+++ Social_N/app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :login?, only: [:show, :edit]
-  load_and_authorize_resource
   
-
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access denied."
     redirect_to main_app.root_path
@@ -16,12 +14,18 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-  	#session[:user_id] = nil
-  		
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def current_admin?
+    current_user.admin?
+  end
+
+  def authenticate_admin_user!
+    redirect_to root_path unless current_admin?
+  end
+
+  helper_method :current_user, :current_admin?
 end
 
 
